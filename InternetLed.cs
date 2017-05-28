51210cfd77ca76e378298b30547c298e3fd42c21
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2017-05-29 00:14:43 +0300

    heavy refactoring and bugfixes

--- InternetLed.cs
+++ InternetLed.cs
@@ -2,140 +2,24 @@
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Windows.Devices.Gpio;
 
 namespace Lesson_201
 {
-    /// <summary>
-    /// A class which wraps a GPIO interfaced LED which can get it's display state set based on
-    /// a web API.
-    /// </summary>
     class InternetLed
     {
-        // GPIO controller code
-        private GpioController gpio;
-        private GpioPin LedControlGPIOPin;
-        private int LedControlPin;
-
-        // only used if we don't get a response from the webapi call.
-        private const int DefaultBlinkDelay = 50;
-
-        // An enumeration to store the state of the led
-        public enum eLedState { Off, On };
-        private eLedState _LedState;
-
         private int throttle = 0;
 
         public int getThrottle()
         {
             return throttle;
         }
-
-        //public InternetLed(int ledControlPin)
-        //{
-        //    Debug.WriteLine("InternetLed::New InternetLed");
-
-        //    // Store the selected GPIO control pin id for use when we initialize the GPIO
-        //    LedControlPin = ledControlPin;
-        //}
-
-
+        
         public void InitalizeLed()
         {
-            Debug.WriteLine("InternetLed::InitalizeLed");
-
-            // Now setup the LedControlPin
-            gpio = GpioController.GetDefault();
-
-            //LedControlGPIOPin = gpio.OpenPin(LedControlPin);
-            //LedControlGPIOPin.SetDriveMode(GpioPinDriveMode.Output);
-
-            // Get the current pin value
-            //GpioPinValue startingValue = LedControlGPIOPin.Read();
-            //_LedState = (startingValue == GpioPinValue.Low) ? eLedState.On : eLedState.Off;
+            Debug.WriteLine("InternetLed::InitalizeLed");            
         }
-
-        // A public property for interacting with the LED from code.
-        public eLedState LedState
-        {
-            get { return _LedState; }
-            set
-            {
-                Debug.WriteLine("InternetLed::LedState::set " + value.ToString());
-                if (LedControlGPIOPin != null)
-                {
-                    GpioPinValue newValue = (value == eLedState.On ? GpioPinValue.High : GpioPinValue.Low);
-                    LedControlGPIOPin.Write(newValue);
-                    _LedState = value;
-                }
-            }
-        }
-
-
-
-        // Change the state of the led from on to off or off to on
-        public void Blink()
-        {
-            if (LedState == eLedState.On)
-            {
-                LedState = eLedState.Off;
-            }
-            else
-            {
-                LedState = eLedState.On;
-            }
-        }
-
-        public void Drive()
-        {
-            if (throttle == 100)
-            {
-                LedControlGPIOPin.Write(GpioPinValue.High);
-            } 
-            else
-            {
-                LedControlGPIOPin.Write(GpioPinValue.Low);
-
-            }
-        }
-
-        
-        // This will call an exposed web API at the indicated URL
-        // the API will return a string value to use as the blink delay.
-        const string WebAPIURL = "http://192.168.1.64:8080/rest/blinker";
-        //const string WebAPIURL = "http://adafruitsample.azurewebsites.net/TimeApi";
-        public async Task<int> GetBlinkDelayFromWeb()
-        {
-            Debug.WriteLine("InternetLed::MakeWebApiCall");
-
-            string responseString = "No response";
-
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    // Make the call
-                    responseString = await client.GetStringAsync(WebAPIURL);
-
-                    // Let us know what the returned string was
-                    Debug.WriteLine(String.Format("Response string: [{0}]", responseString));
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.Message);
-            }
-
-            int delay;
-
-            if (!int.TryParse(responseString, out delay))
-            {
-                delay = DefaultBlinkDelay;
-            }
-
-            // return the blink delay
-            return delay;
-        }
+                        
+        const string WebAPIURL = "http://metabox.dynu.com:8080/rest/blinker";
 
         public async Task<int> GetThrottleFromWeb()
         {
@@ -147,10 +31,8 @@ namespace Lesson_201
             {
                 using (HttpClient client = new HttpClient())
                 {
-                    // Make the call
                     responseString = await client.GetStringAsync(WebAPIURL);
-
-                    // Let us know what the returned string was
+                    
                     Debug.WriteLine(String.Format("Response string: [{0}]", responseString));
                 }
             }
@@ -158,17 +40,13 @@ namespace Lesson_201
             {
                 Debug.WriteLine(e.Message);
             }
-            
+
             if (!int.TryParse(responseString, out throttle))
             {
                 throttle = 0;
             }
-
-            // return the blink delay
             
             return throttle;
         }
-
     }
-
 }
